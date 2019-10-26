fefb47537faff86cf26442b0dcd692e1c9f3dc43
Afletch <austinj.fletcher@gmail.com>
2019-10-26 13:38:54 -0700

    fix integer

--- models/userData.js
+++ models/userData.js
@@ -8,6 +8,7 @@ module.exports = function(sequelize, DataTypes) {
     playerID: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       defaultValue: randomstring.generate({
         length: 14,
         charset: "alphanumeric"
@@ -15,11 +16,16 @@ module.exports = function(sequelize, DataTypes) {
     },
     username: {
       type: DataTypes.STRING,
+      unique: true,
       allowNull: false
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
       type: DataTypes.STRING,
