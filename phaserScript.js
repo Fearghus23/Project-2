8ffd6f1b9b5c8daf0b222543c93e1905c25305bc
eazimms <eric.allen.zimmerman@gmail.com>
2019-11-05 03:44:56 -0800

    semi functioning game, score records, entries are tracked, animation happens, physics works

--- phaserScript.js
+++ phaserScript.js
@@ -2,11 +2,13 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  backgroundColor: '#010101',
+  parent: 'phaser-example',
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
-      debug: true
+      gravity: { y: 5 },
+      debug: false
     }
   },
   scene: {
@@ -15,13 +17,17 @@ var config = {
     update: update
   }
 };
-// eslint-disable-next-line no-unused-vars
-var currentWord = [];
-var input = [];
 
-// eslint-disable-next-line no-unused-vars
+var container;
+var wordArray = ["HTML", "GIT", "CSS", "JAVASCRIPT", "JQUERY", "NODE", "NPM", "INSTALL", "GITHUB",
+  "ESLINT", "COMMIT", "MYSQL", "VSCODE", "ID", "CLASS", "STYLESHEET", "HANDLEBARS", "REACT", "VUE", "SERVER", "SEQUELIZE",
+  "XML", "JSON", "OBJECT", "VARIABLE", "LET", "CONST", "PACKAGE", "TIM"];
+var currentWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+var input = [];
+var inputText;
+var score = 0;
+var scoreText;
 var current;
-// eslint-disable-next-line no-unused-vars
 var gameOver = false;
 var keyQ;
 var keyW;
@@ -49,18 +55,40 @@ var keyV;
 var keyB;
 var keyN;
 var keyM;
-// eslint-disable-next-line no-unused-vars
 var characters;
-// eslint-disable-next-line no-unused-vars
+var container;
+var Enter;
+var speed;
+var Backspace;
+var gameOver = false;
+
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("background", "assets/testspace.jpg");
-  this.load.image("bomb", "assets/bomb.png");
+  this.load.image("background", "assets/testspace.jpg", { frameWidth: 16, frameHeight: 16 });
+  this.load.spritesheet("ship", "assets/ship.png", { frameWidth: 16, frameHeight: 16 });
+  this.load.spritesheet("ship2", "assets/ship2.png", { frameWidth: 32, frameHeight: 16 });
+  this.load.spritesheet("ship3", "assets/ship3.png", { frameWidth: 32, frameHeight: 32 });
+  this.load.spritesheet("explosion", "assets/explosion.png", { frameWidth: 16, frameHeight: 16 })
+  this.load.image("base", "assets/platform.png")
+
 }
 
+
 function create() {
-  // Inputs
+  console.log(currentWord);
+  this.background = this.add.image(100, 100, "background");
+  this.background.setScale(2);
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, "base").setScale(2).refreshBody();
+  scoreText = this.add.text(16, 16, 'score: 0', + { fontSize: '32px', fill: 'blue' });
+  inputText = this.add.text(16, 35, "Current: ", { fontSize: '32px', fill: 'yellow' });
+  container = this.add.text(300, 560, "Keyword: " + currentWord, { fontSize: "25px", fill: "#ffecf4" });
+
+
+
+
+
   keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
   keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
   keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
@@ -87,158 +115,245 @@ function create() {
   keyB = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
   keyN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.N);
   keyM = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
-  // eslint-disable-next-line no-unused-vars
-  var inputText;
-  // eslint-disable-next-line no-unused-vars
-  var score = 0;
-  // eslint-disable-next-line no-unused-vars
-  var scoreText;
+  Enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+  Backspace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKSPACE);
 
-  var image1 = this.add.image(0, -30, "bomb");
-  var image2 = this.add.text(-40, 30, "bomb");
-  var image3 = this.add.image(40, 30, "bomb");
 
-  container = this.add.container(400, 200, [image1, image2, image3]);
 
-  //  A Container has a default size of 0x0, so we need to give it a size before enabling a physics
-  //  body or it'll be given the default body size of 64x64.
-  container.setSize(128, 64);
+  this.ships = this.physics.add.group({
+    key: 'ship3',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
 
-  this.physics.world.enable(container);
 
-  container.body
-    .setVelocity(100, 200)
-    .setBounce(1, 1)
-    .setCollideWorldBounds(true);
+  this.physics.add.collider(this.ships, platforms);
 
-  // Already entered letters:
+  this.physics.add.overlap(platforms, this.ships, null, this);
+
+
+
+
+
 }
 
 function update() {
-  this.add.image(400, 300, "background");
 
-  this.add.image("bomb");
+  if (gameOver) {
+    return;
+  }
 
-  if (container.body.velocity.x < 0) {
-    container.rotation -= 0.02;
-  } else {
-    container.rotation += 0.02;
+
+  function endIt(platform, ships) {
+    this.physic.pause();
+
+    platform.setTint(0xff0000);
+
+    gameOver = true;
+    ship.setTexture("explosion");
+    ship.play("explode");
+  }
+
+  // Display the entered words. 
+
+  inputText.setText("Current: " + input);
+
+
+  // Inputs
+
+  if (Phaser.Input.Keyboard.JustDown(Enter)) {
+    checkLogs(input, currentWord);
+    currentWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    container.setText("Current: " + currentWord);
+
+
+
+
+
   }
 
   if (Phaser.Input.Keyboard.JustDown(keyQ)) {
-    input.push("Q");
+
+    input.push('Q')
     console.log(input);
+
   }
   if (Phaser.Input.Keyboard.JustDown(keyW)) {
-    input.push("W");
+
+    input.push('W');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyE)) {
-    input.push("E");
+
+    input.push('E');
     console.log(input);
+
   }
   if (Phaser.Input.Keyboard.JustDown(keyR)) {
-    input.push("R");
+
+    input.push('R');
     console.log(input);
+
   }
   if (Phaser.Input.Keyboard.JustDown(keyT)) {
-    input.push("T");
+
+    input.push('T');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyY)) {
-    input.push("Y");
+
+    input.push('Y');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyU)) {
-    input.push("U");
+
+    input.push('U');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyI)) {
-    input.push("I");
+
+    input.push('I');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyO)) {
-    input.push("O");
+
+    input.push('O');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyP)) {
-    input.push("P");
+
+    input.push('P');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyA)) {
-    input.push("A");
+
+    input.push('A');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyS)) {
-    input.push("S");
+
+    input.push('S');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyD)) {
-    input.push("D");
+
+    input.push('D');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyF)) {
-    input.push("F");
+
+    input.push('F');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyG)) {
-    input.push("G");
+
+    input.push('G');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyH)) {
-    input.push("H");
+
+    input.push('H');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyJ)) {
-    input.push("J");
+
+    input.push('J');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyK)) {
-    input.push("K");
+
+    input.push('K');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyL)) {
-    input.push("L");
+
+    input.push('L');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyZ)) {
-    input.push("Z");
+
+    input.push('Z');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyX)) {
-    input.push("X");
+
+    input.push('X');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyC)) {
-    input.push("C");
+
+    input.push('C');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyV)) {
-    input.push("V");
+
+    input.push('V');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyB)) {
-    input.push("B");
+
+    input.push('B');
     console.log(input);
   }
   if (Phaser.Input.Keyboard.JustDown(keyN)) {
-    input.push("N");
+
+    input.push('N');
     console.log(input);
-    inputText.setText(input);
+
   }
   if (Phaser.Input.Keyboard.JustDown(keyM)) {
-    input.push("M");
+
+    input.push('M');
     console.log(input);
 
-    inputText.setText(input);
+
   }
 
-  scoreText = this.add.text(16, 16, "score: 0", {
-    fontSize: "32px",
-    fill: "#000"
-  });
-  inputText = this.add.text(16, 35, "Entered Letters: " + input, {
-    fontSize: "32px",
-    fill: "#000"
-  });
+  if (Phaser.Input.Keyboard.JustDown(Backspace)) {
+    input.pop();
+    console.log("removed");
+  }
+
+  function checkLogs(input, currentWord) {
+
+
+
+    var x = input.join("");
+    console.log(x);
+
+    if (x == currentWord) {
+
+
+
+      console.log(input);
+      input.length = 0;
+      console.log('correct');
+
+
+      score += 10;
+      scoreText.setText("Score: " + score);
+
+
+      // ships.disableBody(true, true); 
+
+
+
+
+
+
+    } else {
+      input.length = 0;
+      console.log('NO');
+
+
+    }
+
+
+
+
+  }
+
 }
+
+
+
