db2965aa9461790e5912fcd0bd4a51837363a3ef
Fearghus Tyler <Fearghus333@gmail.com>
2019-11-05 12:11:14 -0800

    updateorganize

--- gameStructure/states/preload.js
+++ gameStructure/states/preload.js
@@ -1,7 +1,27 @@
 Preload.prototype = {
   preload: function() {
-    // load all game assets
-    // images, spritesheets, atlases, audio etc..
+    // Preloaded assets for the game sprites.
+    this.load.image("background", "assets/testspace.jpg", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship", "assets/ship.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship2", "assets/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship3", "assets/ship3.png", {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet("explosion", "assets/explosion.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.image("base", "assets/platform.png");
   },
   create: function() {
     this.game.state.start("main-intro");
